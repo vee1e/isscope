1c8b6a422e6c15950d6c433da8fc0fe1e79bc1bb
Aarya1402 <patelaarya1402@gmail.com>
2026-06-05 11:12:21 +0530

    chore: remove debug files

--- debug_test.js
+++
@@ -1,5 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { InputScreen } from './src/screens/InputScreen';
-import React from 'react';
-import { useAppStore } from './src/store/appStore';
-console.log(useAppStore.getState().history);
